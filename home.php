460146218a8fde0ed1056a9a0ea947b6506ea871
HP <divi21146@gmail.com>
2023-07-06 16:06:07 +0530

    Changed a Format

--- home.php
+++ home.php
@@ -11,74 +11,76 @@ $user_row = mysqli_fetch_array($user);
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>profile</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-  <style>
-    
-  </style>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>profile</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <style>
+
+    </style>
 </head>
+
 <body>
-	<nav class="nav">
-    <a style="font-size: 23px;" onclick="return false;">Welcome <?php echo $user_row['name']; ?> !</a>
-    <a style="padding-left: 40px; padding-right: 40px;" href="home.php">Home</a>
-    <a style="padding-left: 40px; padding-right: 40px;" href="profile.php"></i>Profile</a>
-    <form style="float: right;" method="get">
-    <a><button type="submit" name="logout" class="my-button">logout</button></a>
-    
-  </form>
-  </nav><br>
+    <nav class="nav">
+        <a style="font-size: 23px;" onclick="return false;">Welcome <?php echo $user_row['name']; ?> !</a>
+        <a style="padding-left: 40px; padding-right: 40px;" href="home.php">Home</a>
+        <a style="padding-left: 40px; padding-right: 40px;" href="profile.php"></i>Profile</a>
+        <form style="float: right;" method="get">
+            <a><button type="submit" name="logout" class="my-button">logout</button></a>
+
+        </form>
+    </nav><br>
 
 
- <?php
+    <?php
 $folder="upload/";
 $qry=mysqli_query($conn,"select * from post ORDER BY id DESC");
 while($row1=mysqli_fetch_array($qry)){
 ?>
 
-<div class="card" style="margin-left: 40px; margin-right: 30px;">
- <div class="image"><img src="<?php echo "$folder".$row1['image']; ?>">
+    <div class="card" style="margin-left: 40px; margin-right: 30px;">
+        <div class="image"><img src="<?php echo "$folder".$row1['image']; ?>">
 
-</div>
-  <div class="content">
-      <span style="text-transform: uppercase;" class="title">
-        <?php echo $row1['title']; ?>
-      </span>
+        </div>
+        <div class="content">
+            <span style="text-transform: uppercase;" class="title">
+                <?php echo $row1['title']; ?>
+            </span>
 
-    <div class="scroll">
-    <p class="desc1">
-      <i><?php echo $row1['description']; ?></i>
-    </p>
-  </div>
-    <p class="desc">
-      <?php echo $row1['status']; ?>
-    </p>
+            <div class="scroll">
+                <p class="desc1">
+                    <i><?php echo $row1['description']; ?></i>
+                </p>
+            </div>
+            <p class="desc">
+                <?php echo $row1['status']; ?>
+            </p>
 
-    <a href="detail.php?id=<?php echo $row1['id']?>">
-      <button class="detail" style="vertical-align:middle"><span>Click</span></button>
-    </a><br>
-    <hr>
-    
-    <?php
+            <a href="detail.php?id=<?php echo $row1['id']?>">
+                <button class="detail" style="vertical-align:middle"><span>Click</span></button>
+            </a><br>
+            <hr>
+
+            <?php
     $mail1=$row1['mail'];
     $fetch=mysqli_query($conn,"select * from registration where mail='$mail1'");
     while ($use=mysqli_fetch_array($fetch)) {
     ?>
-    <p class="desc2">
-      <?php echo $row1['reg_date']; ?> | Created by <i> <?php echo $use['name']; ?></i>
-    </p>
-    <?php
+            <p class="desc2">
+                <?php echo $row1['reg_date']; ?> | Created by <i> <?php echo $use['name']; ?></i>
+            </p>
+            <?php
   }
     ?>
-  </div>
-</div>
-<?php 
+        </div>
+    </div>
+    <?php 
 }
 ?>
 
-  <?php
+    <?php
   if(isset($_GET['logout'])){
     
     session_destroy();
@@ -91,4 +93,5 @@ while($row1=mysqli_fetch_array($qry)){
   ?>
 
 </body>
+
 </html>
--- registration.php
+++ registration.php
@@ -29,176 +29,191 @@ if(isset($_POST['submit'])){
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>registration</title>
-	<style>
-		*{
-			font-family: sans-serif;
-		}
-		.container {
-			margin: 130px 360px;
-		}
-		.left{
-			margin: -110px -180px 20px;
-			float: left;
-			background-color: #fff;
-			display: flex;
-			overflow: hidden;
-			width: 500px;
-			height: 650px;
-		}
-		.left img {
-			  width: 100%;
-			  height: 100%;
-			  object-fit: cover;
-			}
-		.bg{
-			float: right;
-			margin: -110px 320px 20px;
-			position: absolute;
-			width: 400px;
-			height: 638px;
-			padding: 10px 50px 0 50px;
-			border: 1px solid black;
-		}
-		.bg h1{
-			margin: 0;
-			color: black;
-			padding-top: 10px;
-			text-align: center;
-			font-style: italic;
-		}
-		.bg label{
-			margin: 0;
-			padding: 0;
-			color: #000;
-		}
-		p{
-			color: #000;
-		}
-		a{
-			color: purple;
-			font-style: italic;
-			font-weight: bold;
-			text-decoration: none;
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>registration</title>
+    <style>
+    * {
+        font-family: sans-serif;
+    }
 
-		}
-		a:hover{
-			color: royalblue;
-		}
-		.bg input{
-			width: 100%;
-			margin-bottom: 20px;
-		}
-		.bg input[type=email],
-		.bg input[type=password],
-		.bg input[type=text],
-		.bg input[type=number],
-		.bg input[type=textarea]{
-			border: none;
-			border-bottom: 1px solid #000;
-			background: transparent;
-			outline: none;
-			height: 35px;
-			color: #000;
-			font-size: 15px;
-		}
-		.my-button {
-  background-color: rgb(24,117,255);
-  border: none;
-  color: white;
-  padding: 10px 30px;
-  text-align: center;
-  /*text-decoration: none;
+    .container {
+        margin: 130px 360px;
+    }
+
+    .left {
+        margin: -110px -180px 20px;
+        float: left;
+        background-color: #fff;
+        display: flex;
+        overflow: hidden;
+        width: 500px;
+        height: 650px;
+    }
+
+    .left img {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    .bg {
+        float: right;
+        margin: -110px 320px 20px;
+        position: absolute;
+        width: 400px;
+        height: 638px;
+        padding: 10px 50px 0 50px;
+        border: 1px solid black;
+    }
+
+    .bg h1 {
+        margin: 0;
+        color: black;
+        padding-top: 10px;
+        text-align: center;
+        font-style: italic;
+    }
+
+    .bg label {
+        margin: 0;
+        padding: 0;
+        color: #000;
+    }
+
+    p {
+        color: #000;
+    }
+
+    a {
+        color: purple;
+        font-style: italic;
+        font-weight: bold;
+        text-decoration: none;
+
+    }
+
+    a:hover {
+        color: royalblue;
+    }
+
+    .bg input {
+        width: 100%;
+        margin-bottom: 20px;
+    }
+
+    .bg input[type=email],
+    .bg input[type=password],
+    .bg input[type=text],
+    .bg input[type=number],
+    .bg input[type=textarea] {
+        border: none;
+        border-bottom: 1px solid #000;
+        background: transparent;
+        outline: none;
+        height: 35px;
+        color: #000;
+        font-size: 15px;
+    }
+
+    .my-button {
+        background-color: rgb(24, 117, 255);
+        border: none;
+        color: white;
+        padding: 10px 30px;
+        text-align: center;
+        /*text-decoration: none;
   display: inline-block;*/
-  font-size: 16px;
-  border-radius: 10px;
-  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
-  position: relative;
-  overflow: hidden;
-  float: right;
-}
+        font-size: 16px;
+        border-radius: 10px;
+        box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
+        position: relative;
+        overflow: hidden;
+        float: right;
+    }
 
-.my-button:after {
-  content: "";
-  background-color: rgba(150,200,255, 0.2);
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  width: 5px;
-  height: 5px;
-  border-radius: 50%;
-  transform: translate(-50%, -50%);
-  opacity: 0;
-}
+    .my-button:after {
+        content: "";
+        background-color: rgba(150, 200, 255, 0.2);
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: 5px;
+        height: 5px;
+        border-radius: 50%;
+        transform: translate(-50%, -50%);
+        opacity: 0;
+    }
 
-.my-button:hover:after {
-  animation: ripple_401 1s ease-out;
-}
+    .my-button:hover:after {
+        animation: ripple_401 1s ease-out;
+    }
 
-@keyframes ripple_401 {
-  0% {
-    width: 5px;
-    height: 5px;
-    opacity: 1;
-  }
+    @keyframes ripple_401 {
+        0% {
+            width: 5px;
+            height: 5px;
+            opacity: 1;
+        }
 
-  100% {
-    width: 200px;
-    height: 200px;
-    opacity: 0;
-  }
-}
-
-	</style>
+        100% {
+            width: 200px;
+            height: 200px;
+            opacity: 0;
+        }
+    }
+    </style>
 </head>
+
 <body>
-	<div class="container">
-		<div class="left"><img src="mount.jpg"></div>
-		<div class="bg">
-			<form method="post" action="registration.php" autocomplete="off">
-		<h1>Registration form</h1><br>
-		<label>Name</label>
-		<input type="text" name="name" placeholder="Enter your name" required><br>
-		<label>Email</label>
-		<input type="email" name="mail" placeholder="Enter your email" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" required><br>
-		<label>Password</label>
-		<input type="password" name="password" placeholder="Enter your password" required><br>
-		<label>Confirm password</label>
-		<input type="password" name="confirm_password" placeholder="Enter your confirm password" required><br>
-		<label>Mobile</label>
-		<input type="number" name="number" id="number" pattern="[0-9]{10}$s" placeholder="Enter your mobile number" required><br>
-		<label>Address</label>
-		<input type="textarea" name="address" placeholder="Enter your address" required><br>
-        <p>Already registered?<a href="login.php">login</a></p>
-		<button type="submit" name="submit" class="my-button">register</button>
-	</form>
-</div>
-</div>
+    <div class="container">
+        <div class="left"><img src="mount.jpg"></div>
+        <div class="bg">
+            <form method="post" action="registration.php" autocomplete="off">
+                <h1>Registration form</h1><br>
+                <label>Name</label>
+                <input type="text" name="name" placeholder="Enter your name" required><br>
+                <label>Email</label>
+                <input type="email" name="mail" placeholder="Enter your email"
+                    pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" required><br>
+                <label>Password</label>
+                <input type="password" name="password" placeholder="Enter your password" required><br>
+                <label>Confirm password</label>
+                <input type="password" name="confirm_password" placeholder="Enter your confirm password" required><br>
+                <label>Mobile</label>
+                <input type="number" name="number" id="number" pattern="[0-9]{10}$s"
+                    placeholder="Enter your mobile number" required><br>
+                <label>Address</label>
+                <input type="textarea" name="address" placeholder="Enter your address" required><br>
+                <p>Already registered?<a href="login.php">login</a></p>
+                <button type="submit" name="submit" class="my-button">register</button>
+            </form>
+        </div>
+    </div>
 
-<script>
-	const numberInput = document.getElementById('number');
+    <script>
+    const numberInput = document.getElementById('number');
 
-numberInput.addEventListener('input', () => {
-  const numberValue = numberInput.value;
-  
-  // Check if the number input contains only digits
-  const isNumberValid = /^\d+$/.test(numberValue);
-  
-  // Check if the number input contains exactly 10 digits
-  const isLengthValid = numberValue.length === 10;
-  
-  // Set the input's validity based on the validation checks
-  numberInput.setCustomValidity(
-    isNumberValid && isLengthValid ? '' : 'Please enter a 10-digit number'
-  );
-  
-  // Update the input's UI to show any validation errors
-  numberInput.reportValidity();
-});
+    numberInput.addEventListener('input', () => {
+        const numberValue = numberInput.value;
 
-</script>
+        // Check if the number input contains only digits
+        const isNumberValid = /^\d+$/.test(numberValue);
+
+        // Check if the number input contains exactly 10 digits
+        const isLengthValid = numberValue.length === 10;
+
+        // Set the input's validity based on the validation checks
+        numberInput.setCustomValidity(
+            isNumberValid && isLengthValid ? '' : 'Please enter a 10-digit number'
+        );
+
+        // Update the input's UI to show any validation errors
+        numberInput.reportValidity();
+    });
+    </script>
 </body>
+
 </html>
